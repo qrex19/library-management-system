318b0b6dd55a036010e5f90c0810d9bba2a5f244
qrex19 <nishanttjhaa25@gmail.com>
2023-12-26 22:01:45 +0530

    improv: functionalities added

--- src/http-library-server.js
+++ src/http-library-server.js
@@ -1,1 +1,28 @@
-console.log("hello world!!");
+const express = require("express");
+const app = express();
+const port = 3000; //port
+
+/**
+ * functionalities:
+ * -add a book(title, description, issueStatus, author)!(post)
+ * -generate a bookId associated to every book stored!(functionality)
+ * -remove a book!(delete)
+ * -change book's issue status!(put)
+ * -retrieve all book's of an author in the library(get)
+ */
+
+//dataBase
+let books = [
+  {
+    title: "the book",
+    description: "the description",
+    issueStatus: false,
+    authorName: "arundhati roy",
+  },
+];
+
+app.post("/add", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
